05e65531cfeee8ce289df8b1c33c973b3a40616d
nururuv <nuru.w121@gmail.com>
2022-02-23 14:28:30 +0900

    amplify authの追加 サインイン追加

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nextjsamplify0eb84499": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Layout from "../components/Layout";
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-export default function Home() {
+import awsconfig from "../src/aws-exports";
+Amplify.configure(awsconfig);
+
+function Home() {
   return (
     <Layout title="Json">
       <h1>Hello World</h1>
     </Layout>
   );
 }
+
+export default withAuthenticator(Home);
